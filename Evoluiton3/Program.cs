0ddfd34ad0f04720e473ea6c288f49a45c40c41a
Everton Paulino <evertonpaulino25@gmail.com>
2022-10-27 15:19:48 -0300

    Adicionar arquivos de projeto.

---
+++ Evoluiton3/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+
+namespace Evoluiton3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string valor1 = RemoverCaracteresDuplicados("abracadabra, allottee, assessee");
+            string valor2 = RemoverCaracteresDuplicados("kelless, keenness, Alfalggo");
+            
+            Console.WriteLine(valor1);
+            Console.WriteLine(valor2);
+            
+        }
+
+        static string RemoverCaracteresDuplicados(string texto)
+        {
+            // Armazena as letras 
+            string tabela = "";
+            // armazena o resultado
+            string resultado = "";
+            // Percorre cada caractere do texto
+            foreach (char valor in texto)
+            {
+                // Verifica se o caractere esta na tabela
+                if (tabela.IndexOf(valor) == -1)
+                {
+                    // anexa as letras na tabela e no resultado
+                    tabela += valor;
+                    resultado += valor;
+                }
+            }
+            return resultado;
+        }
+    }
+}
+    
+
